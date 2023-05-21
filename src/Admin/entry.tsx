4c28d3ed7234c8d957b0c862d80b9fa81640e441
dominik <dominik.jelinek111@gmail.com>
2023-05-21 21:50:19 +0100

    Add Apache Echarts and couple example

--- src/Admin/entry.tsx
+++ src/Admin/entry.tsx
@@ -7,6 +7,7 @@ import { MenuItem } from "primereact/menuitem";
 import { PanelMenu } from "primereact/panelmenu";
 import { Button } from "primereact/button";
 import { TabMenu } from "primereact/tabmenu";
+import ReactECharts from "echarts-for-react";
 
 import "primereact/resources/themes/mira/theme.css";
 import "primereact/resources/primereact.min.css";
@@ -163,6 +164,306 @@ function MultipleDemo() {
   );
 }
 
+function Chart() {
+  const option = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        // Use axis to trigger tooltip
+        type: "shadow", // 'shadow' as default; can also be 'line' or 'shadow'
+      },
+    },
+    legend: {},
+    grid: {
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      containLabel: true,
+    },
+    xAxis: {
+      type: "value",
+    },
+    yAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    series: [
+      {
+        name: "Direct",
+        type: "bar",
+        stack: "total",
+        label: {
+          show: true,
+        },
+        emphasis: {
+          focus: "series",
+        },
+        data: [320, 302, 301, 334, 390, 330, 320],
+      },
+      {
+        name: "Mail Ad",
+        type: "bar",
+        stack: "total",
+        label: {
+          show: true,
+        },
+        emphasis: {
+          focus: "series",
+        },
+        data: [120, 132, 101, 134, 90, 230, 210],
+      },
+      {
+        name: "Affiliate Ad",
+        type: "bar",
+        stack: "total",
+        label: {
+          show: true,
+        },
+        emphasis: {
+          focus: "series",
+        },
+        data: [220, 182, 191, 234, 290, 330, 310],
+      },
+      {
+        name: "Video Ad",
+        type: "bar",
+        stack: "total",
+        label: {
+          show: true,
+        },
+        emphasis: {
+          focus: "series",
+        },
+        data: [150, 212, 201, 154, 190, 330, 410],
+      },
+      {
+        name: "Search Engine",
+        type: "bar",
+        stack: "total",
+        label: {
+          show: true,
+        },
+        emphasis: {
+          focus: "series",
+        },
+        data: [820, 832, 901, 934, 1290, 1330, 1320],
+      },
+    ],
+  };
+
+  return <ReactECharts option={option} />;
+}
+
+function Chart2() {
+  const app = { configParameters: {}, config: {} };
+
+  // let option;
+
+  const posList = [
+    "left",
+    "right",
+    "top",
+    "bottom",
+    "inside",
+    "insideTop",
+    "insideLeft",
+    "insideRight",
+    "insideBottom",
+    "insideTopLeft",
+    "insideTopRight",
+    "insideBottomLeft",
+    "insideBottomRight",
+  ];
+  app.configParameters = {
+    rotate: {
+      min: -90,
+      max: 90,
+    },
+    align: {
+      options: {
+        left: "left",
+        center: "center",
+        right: "right",
+      },
+    },
+    verticalAlign: {
+      options: {
+        top: "top",
+        middle: "middle",
+        bottom: "bottom",
+      },
+    },
+    position: {
+      options: posList.reduce((map, pos) => {
+        // eslint-disable-next-line no-param-reassign
+        map[pos] = pos;
+        return map;
+      }, {}),
+    },
+    distance: {
+      min: 0,
+      max: 100,
+    },
+  };
+  app.config = {
+    rotate: 90,
+    align: "left",
+    verticalAlign: "middle",
+    position: "insideBottom",
+    distance: 15,
+    onChange() {
+      // const labelOption = {
+      //   // @ts-expect-error: ss
+      //   rotate: app.config.rotate,
+      //   // @ts-expect-error: ss
+      //   align: app.config.align,
+      //   // @ts-expect-error: ss
+      //   verticalAlign: app.config.verticalAlign,
+      //   // @ts-expect-error: ss
+      //   position: app.config.position,
+      //   // @ts-expect-error: ss
+      //   distance: app.config.distance,
+      // };
+    },
+  };
+  const labelOption = {
+    show: true,
+    // @ts-expect-error: ss
+    position: app.config.position,
+    // @ts-expect-error: ss
+    distance: app.config.distance,
+    // @ts-expect-error: ss
+    align: app.config.align,
+    // @ts-expect-error: ss
+    verticalAlign: app.config.verticalAlign,
+    // @ts-expect-error: ss
+    rotate: app.config.rotate,
+    formatter: "{c}  {name|{a}}",
+    fontSize: 16,
+    rich: {
+      name: {},
+    },
+  };
+  const option = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "shadow",
+      },
+    },
+    legend: {
+      data: ["Forest", "Steppe", "Desert", "Wetland"],
+    },
+    toolbox: {
+      show: true,
+      orient: "vertical",
+      left: "right",
+      top: "center",
+      feature: {
+        mark: { show: true },
+        dataView: { show: true, readOnly: false },
+        magicType: { show: true, type: ["line", "bar", "stack"] },
+        restore: { show: true },
+        saveAsImage: { show: true },
+      },
+    },
+    xAxis: [
+      {
+        type: "category",
+        axisTick: { show: false },
+        data: ["2012", "2013", "2014", "2015", "2016"],
+      },
+    ],
+    yAxis: [
+      {
+        type: "value",
+      },
+    ],
+    series: [
+      {
+        name: "Forest",
+        type: "bar",
+        barGap: 0,
+        label: labelOption,
+        emphasis: {
+          focus: "series",
+        },
+        data: [320, 332, 301, 334, 390],
+      },
+      {
+        name: "Steppe",
+        type: "bar",
+        label: labelOption,
+        emphasis: {
+          focus: "series",
+        },
+        data: [220, 182, 191, 234, 290],
+      },
+      {
+        name: "Desert",
+        type: "bar",
+        label: labelOption,
+        emphasis: {
+          focus: "series",
+        },
+        data: [150, 232, 201, 154, 190],
+      },
+      {
+        name: "Wetland",
+        type: "bar",
+        label: labelOption,
+        emphasis: {
+          focus: "series",
+        },
+        data: [98, 77, 101, 99, 40],
+      },
+    ],
+  };
+
+  return <ReactECharts option={option} />;
+}
+
+function Rose() {
+  const option = {
+    legend: {
+      top: "bottom",
+    },
+    toolbox: {
+      show: true,
+      feature: {
+        mark: { show: true },
+        dataView: { show: true, readOnly: false },
+        restore: { show: true },
+        saveAsImage: { show: true },
+      },
+    },
+    series: [
+      {
+        name: "Nightingale Chart",
+        type: "pie",
+        radius: [50, 250],
+        center: ["50%", "50%"],
+        roseType: "area",
+        itemStyle: {
+          borderRadius: 8,
+        },
+        data: [
+          { value: 40, name: "rose 1" },
+          { value: 38, name: "rose 2" },
+          { value: 32, name: "rose 3" },
+          { value: 30, name: "rose 4" },
+          { value: 28, name: "rose 5" },
+          { value: 26, name: "rose 6" },
+          { value: 22, name: "rose 7" },
+          { value: 18, name: "rose 8" },
+        ],
+      },
+    ],
+  };
+
+  return <ReactECharts option={option} />;
+}
+
 function App() {
   const items: MenuItem[] = [
     {
@@ -288,14 +589,14 @@ function App() {
     },
   ];
 
-  const start = (
-    <img
-      alt="logo"
-      src="https://primefaces.org/cdn/primereact/images/logo.png"
-      height="40"
-      className="mr-2"
-    />
-  );
+  // const start = (
+  //   <img
+  //     alt="logo"
+  //     src="https://primefaces.org/cdn/primereact/images/logo.png"
+  //     height="40"
+  //     className="mr-2"
+  //   />
+  // );
   const end = <InputText placeholder="Search" type="text" className="w-full" />;
 
   return (
@@ -304,6 +605,7 @@ function App() {
       <div>This is the admin page dd</div>
       <Dropdown
         value={undefined}
+        // eslint-disable-next-line no-console
         onChange={(e) => console.log(e.value)}
         options={[
           { value: "1", name: "One" },
@@ -315,6 +617,9 @@ function App() {
         filter
         className="w-full md:w-14rem"
       />
+      <Chart />
+      <Chart2 />
+      <Rose />
       <MultipleDemo />
       <ControlledDemo />
     </div>
